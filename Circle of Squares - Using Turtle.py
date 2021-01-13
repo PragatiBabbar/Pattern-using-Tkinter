617900e7280245bd452c5ea15ee6ac650659f4b2
Pragati <68048816+PragatiBabbar@users.noreply.github.com>
2021-01-13 15:37:19 +0530

    Update Circle of Squares - Using Turtle.py

--- Circle of Squares - Using Turtle.py
+++ Circle of Squares - Using Turtle.py
@@ -1,11 +1,16 @@
-#                                       ...Circle Of Squares...
+"""   code by @PragatiBabbar   """
 
+#importing module
 import turtle
 
+# storing module in a variable
 pri = turtle.Turtle()
+
+# specifying the speed
 pri.speed(100)
 
-
+# function to create circle using squares 
+# moving the cursor accordingly
 def square(angle):
     pri.forward(100)
     pri.right(angle)
@@ -16,6 +21,6 @@ def square(angle):
     pri.forward(100)
     pri.right(angle + 10)
 
-
+# using loop to generate pattern
 for i in range(36):
     square(90)
